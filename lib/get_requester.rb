88918e8d83d4972fbe08ca207416d5c6406de7a1
Celina Jackson <celinanjackson@gmail.com>
2020-07-26 18:33:49 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
 URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
 def get_response_body
-  uri = URI.parse(URL)
+  uri = URI.parse("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
   response = Net::HTTP.get_response(uri)
   response.body
 end
